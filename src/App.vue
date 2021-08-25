a2c5d6990d0694fd5318429636345dc27a42ad09
Louis <louis.m.newton@hotmail.fr>
2021-08-25 10:41:40 +0200

    Structure

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+<div class="cv">
+    <div id="nav">
+      <router-link to="/">Home</router-link> 
+      <router-link to="/about">About</router-link>
+    </div>
+    <img alt="Vue logo" src="./assets/photo_profil.jpg">
+
+    <section class="main">
+      <router-view/>
+    </section>
   </div>
-  <section class="main">
-    <router-view/>
-  </section>
 </template>
 
 <style lang="scss">
@@ -16,32 +20,55 @@
   box-sizing: border-box;
 }
 
-.main{
-  width: 80vw;
-  height: 80vh;
-  background-color: #ffffff;
-  margin: auto auto;
-}
+
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   background-color: #fca416;
+  padding: 100px;
+  width: 100vw;
+  height: 100vh;
+}
+
+.cv{
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  height: 100%;
+  
 }
 
 #nav {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
   padding: 30px;
+  height: 100%;
+  background-color: gray;
 
   a {
+    margin: 20px 0;
     font-weight: bold;
     color: #2c3e50;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #e6a031;
     }
   }
+}
+
+.main{
+  // width: 100%;
+  height: 100%;
+  background-color: #ffffff;
+}
+
+img{
+  height: 80vh;
+  border: solid gray 3px;
 }
 </style>
